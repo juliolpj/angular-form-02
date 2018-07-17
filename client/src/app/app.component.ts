72417b42bb53b950a3a8d22e46c7cfce3f506d2e
Julio Perez <juliolpj@hotmail.com>
2018-07-17 15:27:27 -0400

    0.1 Crear HTML

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  systemTitle = 'Template Driven Form 0.0.1';
-  systemDescrip = 'Instalar bootstrap';
+  systemTitle = 'Template Driven Form 0.1';
+  systemDescrip = 'Cear HTML';
+
+  formTemas = ['Angular', 'React', 'VueJs'];
 }
